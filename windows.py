a5a4692d9b31ce0cfbedb12d4662432a6342da5a
James <valeness84@gmail.com>
2015-05-08 21:01:46 -0500

    Playing with ctypes to get process id, Mega Fail

--- windows.py
+++ windows.py
@@ -1,10 +1,32 @@
 from win32ui import GetForegroundWindow
+from win32process import GetCurrentProcess
+import wmi
+import ctypes, subprocess
+from ctypes import byref, c_int
 import time
+
+GetWindowText = ctypes.windll.user32.GetWindowTextW
+GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+GetWindowThreadProcessId = ctypes.windll.user32.GetWindowThreadProcessId
+
+c = wmi.WMI()
 
 def test(wait):
 	# We wait 3 seconds so you have time to switch to a different GetWindowText
 	time.sleep(wait)
 	window = GetForegroundWindow()
+	hwnd = window.GetSafeHwnd()
+	
+	length = GetWindowTextLength(hwnd)
+	buff = ctypes.create_unicode_buffer(length + 1)
+	processID = c_int()
+	threadID = GetWindowThreadProcessId(hwnd,byref(processID))
+
+	print(threadID)
+	for i in c.Win32_Process(ProcessID = threadID):
+		print(i.ProcessID)
+
+	#window.SetWindowText("Testing")
 	title = window.GetWindowText()
 	return title
 
